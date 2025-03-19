3859a82eba22e2664e76de039f852b8e9e1650f4
joaoezx <joao.a.freitas0405@gmail.com>
2025-03-19 19:41:08 -0300

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,6 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { LeaveRequestsModule } from './leave-requests/leave-requests.module';
 import { LeavePoliciesModule } from './leave-policies/leave-policies.module';
-import { NotificationsModule } from './notifications/notifications.module';
-import { ReportsModule } from './reports/reports.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
@@ -15,8 +13,6 @@ import { MongooseModule } from '@nestjs/mongoose';
     UsersModule,
     LeaveRequestsModule,
     LeavePoliciesModule,
-    NotificationsModule,
-    ReportsModule,
     MongooseModule.forRoot(
       'mongodb://joaofreitas:<db_password>@cluster0-shard-00-00.qoe5s.mongodb.net:27017,cluster0-shard-00-01.qoe5s.mongodb.net:27017,cluster0-shard-00-02.qoe5s.mongodb.net:27017/?replicaSet=atlas-ggtkmb-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=Cluster0',
     ),
